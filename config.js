3481a1f8a3c84db2a8076daf3e53b85ff9783748
Rina herve <randriamadyrinaherve@gmail.com>
2026-04-11 22:43:55 +0200

    vl

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js – FICHIER IGNORÉ PAR GIT (contient les secrets)
 const CONFIG = {
   ACCESS_CODE: "1234",                     // Code d'accès
-  API_URL: "https://script.google.com/macros/s/AKfycbxTZZ0ESEjHcimWds8FC9u0UUgktZwMa9H7eVG2gTqs0u-AmTn4tk8hpnwgI0LVCGmfJQ/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbyjOz8Dd0Vi3VdTU4TmDhP9-OL3Y-nMey9PzxwnhdWVL066s34FaY6OWymmNO45yrdSZg/exec"
 };
